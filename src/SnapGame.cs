f5366d170e37f08f6d8da0d03dcea80cc5763dc6
Dennis Dao <dennisdao@outlook.com>
2016-03-09 23:44:00 -0800

    Change ChunkFive font in the game

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -10,6 +10,7 @@ namespace CardGames
         {
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
+			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
             SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
         }
 
@@ -56,8 +57,8 @@ namespace CardGames
 			if (top != null)
 			{
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue,"GameFont",0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue,"GameFont", 0, 50);
 				SwinGame.DrawCell (SwinGame.BitmapNamed
 					("Cards"), top.CardIndex, 521, 153);
 			}
